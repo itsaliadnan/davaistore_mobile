86e24ef3674ff573fcfb130e9e23261945531eba
ali adnan <its.ali.adnan@gmail.com>
2025-11-02 21:28:56 +0300

    main

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:davaistore_mobile/core/router/app_router.dart';
+import 'package:davaistore_mobile/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
+      routerConfig: router,
+    );
+  }
+}
